50b36a581f8e29ab19d21dcdd39949fcb6ea0de0
eve0415 <40632436+eve0415@users.noreply.github.com>
2021-04-13 13:31:12 +0900

    Fix database name

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,7 +3,7 @@ const production = {
     host: process.env.DATABASE_HOST,
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DATABASE,
     migrationsRun: true,
     synchronize: true,
 };
